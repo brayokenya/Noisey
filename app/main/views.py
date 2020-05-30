0dbafd2965f2f428294920e1845819e4508d2956
brayokenya <kiirubrian21@gmail.com>
2020-05-30 21:08:37 +0300

    Fixed read article url

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..requests import get_sources, get_articles
+from ..request import get_sources, get_articles
 from ..models import Source
 
 
--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     """
     General configuration parent class
     """
-    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}&apiKey={}'
-    EVERYTHING_SOURCE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&pageSize={}&apiKey={}'
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=fc0dd4871fc54be18c1ae26a02ba8a53'
+    EVERYTHING_SOURCE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=fc0dd4871fc54be18c1ae26a02ba8a53'
 
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
